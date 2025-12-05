mkch/DeMic

commit c770ae83593548a49dd182d231a8b7c56b7bf3e2

Add hide caption feature to Billboard window

TimeDebouncer.h
@@ -3,22 +3,22 @@
 #include <windows.h>
 #include <functional>
 #include <map>
-#include "Log.h"
 
 template<class... Args>
 class TimeDebouncer {
 private:
 	const UINT mInterval = 0;
 	UINT_PTR mTimerID = 0;
-	std::function<void(Args...)> mOnComplete;
+	const std::function<void()> mLogLastError;
+	const std::function<void(Args...)> mOnComplete;
 	std::function<void()> mOnCompleteClosure; // mOnComplete and Args... are captured here.
 private:
 	static std::map<UINT_PTR, TimeDebouncer*> sInstances; // Map of timer IDs to TimeDebouncer instances.
 	static void CALLBACK TimerProc(HWND hwnd, UINT msg, UINT_PTR id, DWORD time) {
-		if (!KillTimer(NULL, id)) {
-			LOG_LAST_ERROR();
-		}
 		auto instance = sInstances[id];
+		if (!KillTimer(NULL, id)) {
+			instance->mLastErrorLogger();
+		}
 		sInstances.erase(id);
 		instance->mTimerID = 0;
 		instance->mOnCompleteClosure();
@@ -26,21 +26,36 @@ private:
 public:
 	// Construct a TimeDebouncer with the specified interval and onComplete function.
 	// The onComplete function is called when the interval elapses without Emit being called again.
-	TimeDebouncer(UINT interval, const std::function<void(Args...)>& onComplete)
-		: mInterval(interval), mOnComplete(onComplete) {}
+	// The logLastErrorFunc will be called to log the error message of GetLastError() when SetTimer or KillTimer fails.
+	TimeDebouncer(UINT interval, const std::function<void(Args...)>& onComplete, const std::function<void()>& logLastErrorFunc)
+		: mInterval(interval), mOnComplete(onComplete), mLogLastError(logLastErrorFunc){}
+
+	~TimeDebouncer() {
+		Cancel();
+	}
 
 	void Emit(Args... args) {
 		// SetTimer will replace the timer if time id is not zero,
 		// so we don't need to kill the previous timer.
 		mTimerID = SetTimer(NULL, mTimerID, mInterval, TimerProc);
 		if (!mTimerID) {
-			LOG_LAST_ERROR();
+			mLogLastError();
 			return;
 		}
 		mOnCompleteClosure = [this, args...]() {
 			return mOnComplete(args...);
 			};
 		sInstances[mTimerID] = this;
+	}
+
+	void Cancel() {
+		if (mTimerID) {
+			if (!KillTimer(NULL, mTimerID)) {
+				mLogLastError();
+			}
+			sInstances.erase(mTimerID);
+			mTimerID = 0;
+		}
 	}
 };
 

Billboard/Billboard.cpp
@@ -19,10 +19,8 @@ static const wchar_t* const CONFIG_FILE_NAME = L"Billboard.json";
 std::wstring configFilePath;
 
 bool alwaysOnTop = false;
+bool hideCaption = false;
 RECT lastWindowRect = { 0 };
-
-void ReadConfig();
-void WriteConfig();
 
 BOOL WINAPI DllMain(
     HINSTANCE hinstDLL,		// handle to DLL module
@@ -63,6 +61,7 @@ const static char* const CONFIG_LEFT = "left";
 const static char* const CONFIG_TOP = "top";
 const static char* const CONFIG_RIGHT = "right";
 const static char* const CONFIG_BOTTOM = "bottom";
+const static char* const CONFIG_HIDE_CAPTION = "HideCaption";
 
 static std::wstring_convert<std::codecvt_utf8<wchar_t>> wstrconv;
 
@@ -78,6 +77,7 @@ void ReadConfig() {
 		alwaysOnTop = config[CONFIG_ALWAYS_ON_TOP];
 		const auto lastRect = config[CONFIG_LAST_RECT];
 		lastWindowRect = {lastRect[CONFIG_LEFT], lastRect[CONFIG_TOP], lastRect[CONFIG_RIGHT], lastRect[CONFIG_BOTTOM]};
+		hideCaption = config.value(CONFIG_HIDE_CAPTION, false); // CONFIG_HIDE_CAPTION is added later, so use value() with default.
 	} catch (...) {
 		ShowError(plugin.Name, strRes->Load(IDS_READ_CONFIG_FAILED).c_str());
 	}
@@ -92,7 +92,8 @@ void WriteConfig() {
 				{CONFIG_RIGHT, lastWindowRect.right},
 				{CONFIG_BOTTOM, lastWindowRect.bottom},
 			} 
-		} 
+		},
+		{CONFIG_HIDE_CAPTION, hideCaption},
 	};
 	std::ofstream out(configFilePath);
 	out << std::setw(2) << config;

Billboard/Billboard.h
@@ -7,6 +7,7 @@ extern StringRes* strRes;
 extern DeMic_Host* host;
 extern void* state;
 extern bool alwaysOnTop;
+extern bool hideCaption;
 extern RECT lastWindowRect;
 
 void ReadConfig();

Billboard/Billboard.vcxproj
@@ -178,6 +178,7 @@
   </ItemDefinitionGroup>
   <ItemGroup>
     <ClInclude Include="..\sdk\DemicPluginUtil.h" />
+    <ClInclude Include="..\TimeDebouncer.h" />
     <ClInclude Include="Billboard.h" />
     <ClInclude Include="BillboardWnd.h" />
     <ClInclude Include="framework.h" />

Billboard/Billboard.vcxproj.filters
@@ -33,6 +33,9 @@
     <ClInclude Include="..\sdk\DemicPluginUtil.h">
       <Filter>Header Files</Filter>
     </ClInclude>
+    <ClInclude Include="..\TimeDebouncer.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="Billboard.cpp">

Billboard/BillboardWnd.cpp
@@ -2,6 +2,7 @@
 #include "resource.h"
 #include "Billboard.h"
 #include "BillboardWnd.h"
+#include "../TimeDebouncer.h"
 
 static const wchar_t* szWindowClass = L"DeMic-Billboard";
 static const COLORREF WND_BK_COLOR = COLOR_WINDOW;
@@ -14,10 +15,18 @@ static HBRUSH hWndBkBrush = NULL;
 
 void Paint(HWND hWnd, HDC hDC);
 void SyncTopMost(HWND hwnd);
+void SyncHideCaption(HWND hwnd);
 void RestoreLastWindowRect(HWND hwnd);
 
 static RECT rcBeforeMinized = { 0 };
 
+// Start simulating window dragging.
+static void startDrag(HWND hWnd) {
+    SendMessage(hWnd, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+}
+
+static bool leftButtonDown = false;
+static bool inDragging = false;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_CREATE:
@@ -27,6 +36,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         hBmpMuted = LoadBitmapW(hInstance, MAKEINTRESOURCEW(IDB_MICROPHONE_MUTED));
         hWndBkBrush = CreateSolidBrush(GetSysColor(WND_BK_COLOR));
         SyncTopMost(hWnd);
+		SyncHideCaption(hWnd);
         break;
     case WM_DESTROY:
         WriteConfig();
@@ -37,11 +47,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
     case WM_CLOSE:
         ShowWindow(hWnd, SW_HIDE);
         return 0;
-    case WM_LBUTTONDOWN: {
+    case WM_LBUTTONDOWN:
+        leftButtonDown = true;
+        break;
+    case WM_LBUTTONUP:{
+        leftButtonDown = false;
         static const int PADDING = 4;
         RECT rect = { 0 };
         GetClientRect(hWnd, &rect);
-        InflateRect(&rect, -PADDING*2, -PADDING*2);
+        InflateRect(&rect, -PADDING * 2, -PADDING * 2);
         POINT pt = { 0 };
         pt.x = LOWORD(lParam);
         pt.y = HIWORD(lParam);
@@ -50,6 +64,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
         }
         break;
     }
+    case WM_MOUSEMOVE:
+        if(hideCaption && leftButtonDown && !inDragging) {
+			inDragging = true;
+            startDrag(hWnd);
+            return 0;
+		}
+        break;
+    case WM_NCHITTEST:
+        if (hideCaption && inDragging) {
+            // The WM_NCHITTEST message right after the last dragging WM_MOUSEMOVE.
+            inDragging = leftButtonDown = false;
+        }
+        break;
+    case WM_KILLFOCUS:
+        if (hideCaption) {
+			// Just for safety, reset the dragging state when the window loses focus.
+            inDragging = leftButtonDown = false;
+        }
+        break;
     case WM_RBUTTONDOWN: {
         const auto hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_CONTEXT_MENU));
         if (!hMenu) {
@@ -62,6 +95,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             LOG_LAST_ERROR(host, state);
         }
         CheckMenuItem(popupMenu, ID_ALWAYS_ON_TOP, alwaysOnTop ? MF_CHECKED : MF_UNCHECKED);
+		CheckMenuItem(popupMenu, ID_HIDE_CAPTION, hideCaption ? MF_CHECKED : MF_UNCHECKED);
         POINT pt = { 0 };
         GetCursorPos(&pt);
         UINT_PTR cmd = TrackPopupMenu(popupMenu,
@@ -69,10 +103,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             pt.x, pt.y,
             0,
             hWnd, NULL);
-        if (cmd == ID_ALWAYS_ON_TOP) {
+        switch (cmd) {
+        case ID_ALWAYS_ON_TOP:
             alwaysOnTop = !alwaysOnTop;
             SyncTopMost(hWnd);
             WriteConfig();
+            break;
+        case ID_HIDE_CAPTION:
+            hideCaption = !hideCaption;
+            SyncHideCaption(hWnd);
+            WriteConfig();
+			break;
+        case ID_CLOSE:
+            SendMessage(hWnd, WM_CLOSE, 0, 0);
+			break;
         }
         DestroyMenu(hMenu);
         break;
@@ -106,6 +150,23 @@ void RestoreLastWindowRect(HWND hwnd) {
 // Sync the top most state with alwyasOnTop flag.
 void SyncTopMost(HWND hwnd) {
        SetWindowPos(hwnd, alwaysOnTop ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+}
+
+static void HideWindowCaption(HWND hWnd) {
+    SetWindowLongPtr(hWnd, GWL_STYLE, GetWindowLongPtr(hWnd, GWL_STYLE) & ~WS_CAPTION);
+    SetWindowPos(hWnd, NULL, 0, 0, 0, 0,
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+
+static void ShowWindowCaption(HWND hWnd) {
+    SetWindowLongPtr(hWnd, GWL_STYLE, GetWindowLongPtr(hWnd, GWL_STYLE) | WS_CAPTION);
+    SetWindowPos(hWnd, NULL, 0, 0, 0, 0,
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+
+// Sync the caption state with hideCaption flag.
+void SyncHideCaption(HWND hwnd) {
+	hideCaption ? HideWindowCaption(hwnd) : ShowWindowCaption(hwnd);
 }
 
 // Paints the window.

Billboard/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Billboard.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Billboard.rc 使用
 //
 #define IDS_OPEN_BILLBOARD              101
 #define IDS_BILLBOARD                   102
@@ -16,13 +16,20 @@
 #define ID_FILE_40001                   40001
 #define ID_ALWAYS_ON_TOP                40002
 #define ID_FILE_ALWAYSONTOP             40003
+#define ID_40004                        40004
+#define ID_FILE_HIDETITLEBAR            40005
+#define ID_HIDE_CAPTION                 40006
+#define ID_40007                        40007
+#define ID_CLOSE                        40008
+#define ID_FILE_CLOSE                   40009
+#define ID_CLOSE_WINDOW                 40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif

commit ed9764dca8f2258a41125e0ba33df4db32323d03

Fix error logging in TimerProc callback
fix the bug introduced in c770ae83593548a49dd182d231a8b7c56b7bf3e2

TimeDebouncer.h
@@ -17,7 +17,7 @@ private:
 	static void CALLBACK TimerProc(HWND hwnd, UINT msg, UINT_PTR id, DWORD time) {
 		auto instance = sInstances[id];
 		if (!KillTimer(NULL, id)) {
-			instance->mLastErrorLogger();
+			instance->mLogLastError();
 		}
 		sInstances.erase(id);
 		instance->mTimerID = 0;