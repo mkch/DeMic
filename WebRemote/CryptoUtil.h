47f54304e870453b0cc87b16d34e3b6bfcff7de2
Kevin Yuan <farproc@gmail.com>
2026-04-27 13:39:40 +0800

    Use OpenSSL RAND_bytes for GenerateRandomCode

--- WebRemote/CryptoUtil.h
+++ WebRemote/CryptoUtil.h
@@ -1,15 +1,30 @@
 #pragma once
 #include <string>
-#include <random>
-#include <chrono>
-#include <sstream>
+#include <vector>
+#include <stdexcept>
+#include <limits>
+#include <openssl/rand.h>
+
+#undef max
 
 template<class CharT>
 static std::basic_string<CharT> GenerateRandomCode(const std::basic_string_view<CharT> candidateChars, size_t codeLength) {
-	std::random_device rd;
-	std::basic_stringstream<CharT> code;
-	for (size_t i = 0; i < codeLength; i++) {
-		code << candidateChars[rd() % candidateChars.size()];
+	if(codeLength == 0) {
+		throw std::invalid_argument("codeLength must be greater than 0");
 	}
-	return code.str();
+	if (candidateChars.size() > 256) {
+		throw std::invalid_argument("candidateChars.size() must not exceed 256");
+	}
+	if (codeLength > static_cast<size_t>(std::numeric_limits<int>::max())) {
+		throw std::invalid_argument("codeLength is too large");
+	}
+	std::vector<unsigned char> buf(codeLength);
+	if (RAND_bytes(buf.data(), static_cast<int>(codeLength)) != 1) {
+		throw std::runtime_error("RAND_bytes failed");
+	}
+	std::basic_string<CharT> code(codeLength, CharT{});
+	for (size_t i = 0; i < codeLength; i++) {
+		code[i] = candidateChars[buf[i] % candidateChars.size()];
+	}
+	return code;
 }
